4c79ab68aff2a6120f2cd529f8b3ba17129f1bb5
david espinal <despinal0425@gmail.com>
2021-04-28 15:42:25 -0400

    did a get request where an array of even number is catched

--- server.js
+++ server.js
@@ -16,3 +16,45 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.json('pong')
 })
+
+app.get('/greet/:name', async (req, res) => {
+  try {
+    const { name } = req.params;
+    res.send("Why hello there, " + name + "!");
+  } catch(e) {
+    console.log(e);
+    res.send("Produc not found")
+  }
+})
+
+
+app.get('/five', async (req, res) => {
+  try {
+    let array = [];
+    for (let i = 1; i < 6; i++) {
+        array.push(i);
+    }
+    res.json(array);
+  } catch(e) {
+    console.log(e);
+    res.send("Produc not found")
+  }
+})
+
+app.get('/evens/:n', async (req, res) => {
+  try {
+    const { n } = req.params;
+    const evenNumber = parseInt(n)
+
+    let array = [];
+    for (let i = 0; i < evenNumber; i++) {
+      if (i % 2 === 0) {
+        array.push(i);
+      }
+    }
+    res.json(array);
+  } catch(e) {
+    console.log(e);
+    res.send("Produc not found")
+  }
+})
